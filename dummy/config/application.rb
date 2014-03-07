ac4fc9ab0a84df08f9115c4e49ea9e168fff6c29
Mathias Gawlista <gawlista.mathias@vimn.com>
2014-03-07 09:08:42 +0100

    Set default generator ORM to ActiveRecord.

--- dummy/config/application.rb
+++ dummy/config/application.rb
@@ -22,6 +22,10 @@ module Dummy
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
+    config.generators do |g|
+      g.orm :active_record
+    end
   end
 end
 
