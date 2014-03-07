1a96f523c5a99e8b522b669a6d9e6f389afc5258
Mathias Gawlista <gawlista.mathias@vimn.com>
2014-03-07 21:57:24 +0100

    Adds routes for scholarship programs. refs #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   get '/products/scholarship' => 'product/scholarship#index', as: 'scholarship_product'
+  
+  namespace :scholarship do
+    resources :programs
+  end
+  
+  resources :organizations do
+    namespace :scholarship do
+      resources :programs, only: [:index, :new]
+    end
+  end
 end
