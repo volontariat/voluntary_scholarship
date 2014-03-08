56a8a00525edd44a8c75a6c771403cc07e9c2230
Mathias Gawlista <gawlista.mathias@vimn.com>
2014-03-08 14:31:22 +0100

    Sync with voluntary. refs #6

--- dummy/features/step_definitions/user_steps.rb
+++ dummy/features/step_definitions/user_steps.rb
@@ -1,4 +1,8 @@
 Given /^a user named "([^\"]*)"$/ do |name|
-  @me = Factory(:user, name: name, email: "#{name}@volontari.at")
+  @me = FactoryGirl.create(:user, name: name, email: "#{name.gsub(' ', '_')}@volontari.at")  
   @me.reload
+end
+
+Given /^current user has role "([^\"]*)"$/ do |name|
+  @me.roles << Role.find_or_create_by_name(name)
 end
