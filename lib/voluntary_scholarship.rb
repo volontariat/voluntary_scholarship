1256dc4dcbc4bbb4fcc940934c818fd85c82a96a
Mathias Gawlista <gawlista.mathias@vimn.com>
2014-03-10 20:23:10 +0100

    Require model concern. #1

--- lib/voluntary_scholarship.rb
+++ lib/voluntary_scholarship.rb
@@ -2,6 +2,7 @@ require 'voluntary'
 
 require 'voluntary_scholarship/ability'
 require 'voluntary_scholarship/navigation'
+require 'voluntary_scholarship/concerns/model/has_scholarship_programs'
 
 require "voluntary_scholarship/engine"
 
