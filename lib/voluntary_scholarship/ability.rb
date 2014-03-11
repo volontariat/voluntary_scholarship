37dc5a07ff38a5acd263bb825061dc705af2d590
Mathias Gawlista <gawlista.mathias@vimn.com>
2014-03-11 18:26:53 +0100

    Authorization for scholarship iterations. refs #2

--- lib/voluntary_scholarship/ability.rb
+++ lib/voluntary_scholarship/ability.rb
@@ -2,10 +2,11 @@ module VoluntaryScholarship
   class Ability
     def self.after_initialize
       Proc.new do |ability, user, options|
-        ability.can :read, [Scholarship::Program]
+        ability.can :read, [Scholarship::Program, Scholarship::Iteration]
         
         if user.present?
-          ability.can(:restful_actions, Scholarship::Program) {|program| program.organization.try(:user_id) == user.id }
+          ability.can(:restful_actions, Scholarship::Program) {|program| program.organization.blank? || program.organization.user_id == user.id }
+          ability.can(:restful_actions, Scholarship::Iteration) {|iteration| iteration.program.blank? || iteration.program.organization.user_id == user.id }
         end
       end
     end
