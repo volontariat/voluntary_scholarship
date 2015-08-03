09891c9395b9d010a51462c9d91fda06d57fccde
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-08-03 19:05:33 +0200

    Create product in migration through product extension class name.

--- dummy/db/migrate/20140307081024_add_scholarship_product.voluntary_scholarship_engine.rb
+++ dummy/db/migrate/20140307081024_add_scholarship_product.voluntary_scholarship_engine.rb
@@ -1,13 +1,13 @@
 # This migration comes from voluntary_scholarship_engine (originally 20140306201232)
 class AddScholarshipProduct < ActiveRecord::Migration
   def up
-    if product = Product.where(name: 'Scholarship').first
+    if Product::Scholarship.first
     else
-      Product.create(name: 'Scholarship', text: 'Dummy') 
+      Product::Scholarship.create(name: 'Scholarship', text: 'Dummy') 
     end
   end
   
   def down
-    product.destroy if product = Product.where(name: 'Scholarship').first
+    product.destroy if product = Product::Scholarship.first
   end
 end
